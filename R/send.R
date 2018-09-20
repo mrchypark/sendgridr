039118ec861eaa4b7b496b364f2f447f72647fcf
Chanyub.Park <mrchypark@gmail.com>
2018-09-20 18:33:36 +0900

    add get_key_id

--- R/send.R
+++ R/send.R
@@ -5,3 +5,15 @@
 send <- function(mail) {
   return(mail)
 }
+
+#' get api_key_id
+#'
+#' @importFrom httr GET add_headers content
+#' @export
+get_key_id <- function() {
+  tar <- "https://api.sendgrid.com/v3/api_keys"
+  ahd <- httr::add_headers("Authorization" = paste0("Bearer ", Sys.getenv("SENDGRID_API")),
+                           "content-type" = "application/json")
+  api_key_id <- httr::content(httr::GET(tar, ahd))
+  return(api_key_id$result)
+}
