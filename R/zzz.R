364bcb9a689e749736990b67914ad7db1f8b34db
vincent guyader <vincent@guyader.pro>
2023-12-07 12:40:43 +0100

    add silent=TRUE in try()
    add silent=TRUE in try() to avoid error in check log

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
   if (Sys.getenv("SENDGRID_API") != "") {
-    try(
+    try(silent=TRUE,
     keyring::key_set_with_value(service = "apikey",
                                 username = "sendgridr",
                                 password = Sys.getenv("SENDGRID_API"))
